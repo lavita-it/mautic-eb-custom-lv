34271e3ea5e8b2dc3660d0e13566d85af07038c0
Andreas Altenburg <andreas@zerooneconsulting.com>
2019-03-30 07:40:36 +0100

    initial commit

---
+++ app/bundles/WebhookBundle/Event/SendWebhookEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\WebhookBundle\Event;
+
+use Joomla\Http\Response;
+use Mautic\LeadBundle\Entity\Lead;
+use Symfony\Component\EventDispatcher\Event;
+
+class SendWebhookEvent extends Event
+{
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var Lead
+     */
+    private $contact;
+
+    /**
+     * @param Response $response
+     */
+    public function __construct(Response $response, Lead $contact)
+    {
+        $this->response  = $response;
+        $this->contact   = $contact;
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
